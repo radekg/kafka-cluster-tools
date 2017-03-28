8a11603cbf3dd42c8b34fa2ff14ed1b22bb4b8b2
radekg <radek@gruchalski.com>
2017-03-28 21:02:26 +0200

    More Java tests, tidy up the test code. Update readme.

--- src/test/java/com/gruchalski/kafka/test/SimpleTypesTest.java
+++ src/test/java/com/gruchalski/kafka/test/SimpleTypesTest.java
@@ -1,33 +1,11 @@
-/*
- * Copyright 2017 Radek Gruchalski
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.gruchalski.kafka.test;
 
 import com.gruchalski.kafka.java8.KafkaCluster;
 import com.gruchalski.kafka.java8.KafkaClusterSafe;
 import com.gruchalski.kafka.java8.KafkaTopicStatus;
-import com.gruchalski.kafka.java8.SerdeRegistry;
-import com.gruchalski.kafka.java8.exceptions.NoDeserializerException;
-import com.gruchalski.kafka.java8.exceptions.NoSerializerException;
 import com.gruchalski.kafka.scala.ConsumedItem;
 import com.gruchalski.kafka.scala.KafkaTopicConfiguration;
 import com.gruchalski.kafka.scala.KafkaTopicCreateResult;
-import com.gruchalski.kafka.test.serializer.java8.concrete.ConcreteJavaMessageImplementation;
-import com.gruchalski.kafka.test.serializer.java8.concrete.JavaConcreteDeserializer;
-import com.gruchalski.kafka.test.serializer.java8.concrete.JavaConcreteSerializer;
 import com.gruchalski.testing.AsyncUtil;
 import junit.framework.TestCase;
 
@@ -40,36 +18,13 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 
-public class ProduceConsumeTest extends TestCase {
-
-    private SerdeRegistry serdes = SerdeRegistry.getInstance();
-
-    @Override
-    protected void setUp() throws Exception {
-        serdes.registerSerializer(ConcreteJavaMessageImplementation.class, new JavaConcreteSerializer());
-        serdes.registerDeserializer(ConcreteJavaMessageImplementation.class, new JavaConcreteDeserializer());
-    }
-
-    public void testSerializerDeserializer() {
-        try {
-            ConcreteJavaMessageImplementation impl = new ConcreteJavaMessageImplementation("unit-test-java");
-            byte[] serialized = serdes.getSerializerFor(impl).serialize("topic", impl);
-            ConcreteJavaMessageImplementation deserialized = serdes.getDeserializerFor(impl.getClass()).deserialize("test", serialized);
-            assertEquals(deserialized.property, impl.property);
-        } catch (NoSerializerException ex) {
-            fail(ex.getMessage());
-        } catch (NoDeserializerException ex) {
-            fail(ex.getMessage());
-        }
-    }
-
+public class SimpleTypesTest extends TestCase {
+    private String aKey = "a test key";
+    private String aValue = "a test value";
     public void testClusterSetup() {
-        KafkaCluster cluster = new KafkaCluster();
-        Optional<KafkaClusterSafe> maybeCluster = cluster.start();
-        maybeCluster.ifPresent(new Consumer<KafkaClusterSafe>() {
+        new KafkaCluster().start().ifPresent(new Consumer<KafkaClusterSafe>() {
             @Override
             public void accept(KafkaClusterSafe kafkaClusterSafe) {
-
                 ArrayList<KafkaTopicConfiguration> topics = new ArrayList<>();
                 topics.add(new KafkaTopicConfiguration(
                         "test-topic",
@@ -78,7 +33,6 @@ public class ProduceConsumeTest extends TestCase {
                         new Properties(),
                         KafkaTopicConfiguration.toRackAwareMode("enforced").get()
                 ));
-
                 CountDownLatch latch = new CountDownLatch(topics.size());
                 CompletableFuture<List<KafkaTopicCreateResult>> topicCreateStatuses = kafkaClusterSafe.cluster.withTopics(topics);
                 topicCreateStatuses.thenAccept(results -> {
@@ -90,43 +44,35 @@ public class ProduceConsumeTest extends TestCase {
                         }
                     }
                 });
-
                 try {
                     latch.await(10000, TimeUnit.MILLISECONDS);
                 } catch (InterruptedException ex) {
                     fail(ex.getMessage());
                 }
-
-                ConcreteJavaMessageImplementation concrete = new ConcreteJavaMessageImplementation("unit-test-concrete");
-
                 try {
-
                     CountDownLatch producerLatch = new CountDownLatch(1);
                     kafkaClusterSafe.cluster.produce(
                             topics.get(0).name(),
-                            concrete).thenAccept(result -> {
+                            aKey,
+                            aValue).thenAccept(result -> {
                         producerLatch.countDown();
                     });
-
                     try {
                         producerLatch.await(10000, TimeUnit.MILLISECONDS);
                     } catch (InterruptedException ex) {
                         fail(ex.getMessage());
                     }
-
                     AsyncUtil.eventually(() -> {
                         try {
+                            Optional<ConsumedItem<String, String>> consumed = kafkaClusterSafe.cluster.consume(
+                                    topics.get(0).name(), String.class, String.class);
 
-                            Optional<ConsumedItem<byte[], ConcreteJavaMessageImplementation>> consumed = kafkaClusterSafe.cluster.consume(
-                                    topics.get(0).name(),
-                                    ConcreteJavaMessageImplementation.class);
-
-                            assertEquals(consumed.get().value().property, concrete.property);
+                            assertEquals(consumed.get().key(), aKey);
+                            assertEquals(consumed.get().value(), aValue);
                         } catch (Throwable t) {
                             fail(t.getMessage());
                         }
                     });
-
                 } catch (Throwable t) {
                     fail(t.getMessage());
                 } finally {
@@ -135,5 +81,4 @@ public class ProduceConsumeTest extends TestCase {
             }
         });
     }
-
 }
