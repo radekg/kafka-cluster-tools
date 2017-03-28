045a472e995c2db48b511883e71ddda893943a89
radekg <radek@gruchalski.com>
2017-03-28 18:21:27 +0200

    1.1.1

---
+++ src/main/java/com/gruchalski/kafka/java8/exceptions/NoSerializerException.java
@@ -0,0 +1,7 @@
+package com.gruchalski.kafka.java8.exceptions;
+
+public class NoSerializerException extends Exception {
+    public NoSerializerException(String message) {
+        super(message);
+    }
+}
